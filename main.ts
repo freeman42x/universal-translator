0676aa0dc8764adc75f69a5c98c1408aafc35dd7
Răzvan Flavius Panda <razvan.panda@gmail.com>
2023-09-15 11:40:18 +0300

    #

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+import { OpenAI } from "https://esm.sh/langchain/llms/openai";
+
 console.log("hi");
+
+const llm = new OpenAI({
+    temperature: 0.9,
+  });
+
+const result = await llm.predict("What would be a good company name for a company that makes colorful socks?");
+
+console.log(result);
+
+console.log("bye");
